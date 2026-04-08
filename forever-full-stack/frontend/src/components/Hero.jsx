7a317b0b95b2b41bda414720f01153ba2492c312
ThayanithiV <111634551+ThayanithiV@users.noreply.github.com>
2026-04-08 10:27:26 +0530

    Update Hero.jsx

--- forever-full-stack/frontend/src/components/Hero.jsx
+++ forever-full-stack/frontend/src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             </div>
       </div>
       {/* Hero Right Side */}
-      <img className='w-full sm:w-1/2' src={assets.Baby_hero.png} alt="" />
+      <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
     </div>
   )
 }
